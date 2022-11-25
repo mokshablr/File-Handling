3c9edfc6bbda8354f50ff19fe452e49e837caffa
Mokshablr <arjunrmaniyani@gmail.com>
2022-11-25 19:45:12 +0530

    Cleaned code

--- num-files.py
+++ num-files.py
@@ -1,11 +1,22 @@
 import os
-dir=input("Enter directory path: ")
-ctr=0
+print('''Enter the number corresponding to the function:
+1. Count number of files given a parameter
+0. Exit''')
+choice=int(input('Enter your choice:'))
+    
+def num_files(dir, sub):
+    ctr=0
 
-for filename in os.listdir(dir):
-    f= os.path.join(dir,filename)
-    if os.path.isfile(f):
-        if f[-3:len(f)+1]==".py":
-            ctr+=1
+    for filename in os.listdir(dir):
+        f= os.path.join(dir,filename)
+        if os.path.isfile(f):
+            if f.endswith(sub):
+                ctr+=1
 
-print("Number of python files:",ctr)
+    print(f"Number of "{sub}" files:",ctr)
+
+if choice==1:
+    dir=input("Enter directory path: ")
+    sub=input('Enter file type: (Eg: "py","jpg")')
+    num_files(dir, sub)    
+    
