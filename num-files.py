7d1996514b389f3869d6f541857bacdba2f3df7d
Mokshablr <arjunrmaniyani@gmail.com>
2022-12-03 19:21:31 +0530

    Added delete files function

--- num-files.py
+++ num-files.py
@@ -1,10 +1,12 @@
 import os
 print('''Enter the number corresponding to the function:
 1. Count number of files given a parameter
+2. Delete files in a directory
 0. Exit''')
-choice=int(input('Enter your choice:'))
+choice=int(input('Enter your choice: '))
+dir=input("Enter directory path: ")
     
-def num_files(dir, sub):
+def num_files(sub):
     ctr=0
 
     for filename in os.listdir(dir):
@@ -15,8 +17,27 @@ def num_files(dir, sub):
 
     print(f"Number of {sub} files:",ctr)
 
-if choice==1:
-    dir=input("Enter directory path: ")
+def del_files():
+    print('''1. Delete based on file-type(Eg:".py", ".jpg")
+0.Exit \n''')
+    c=int(input("Enter your choice: "))
+    if c==1:
+        ctr=0
+        sub=input('Enter file-type(Eg:".py", ".jpg): ')
+        for filename in os.listdir(dir):
+            f= os.path.join(dir,filename)
+            if os.path.isfile(f):
+                if f.endswith(sub):
+                    os.remove(f)
+                    ctr+=1
+        print(ctr, "files have been deleted.")
+
+if choice==0:
+    exit()
+
+elif choice==1:
     sub=input('Enter file type: (Eg: "py","jpg")')
-    num_files(dir, sub)    
-    
+    num_files(sub)
+
+elif choice==2:
+    del_files()
